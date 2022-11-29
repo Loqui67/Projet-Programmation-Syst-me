49c3972c6c87dc7d2aee7b70662eaa9d5437b0c1
Paul Claus <paul.claus@viacesi.fr>
2022-11-29 14:40:18 +0100

    Commentaire sur ConsoleManager EN/FR

--- developpement/LanguageControl.cs
+++ developpement/LanguageControl.cs
@@ -9,44 +9,53 @@ using System.Globalization;
 using Projet_Programmation_Système.developpement;
 
 //Création d'une classe pour gérer les langues.
+//Creation of a class to manage languages.
 public class LanguageControl
 {
     //Création d'une variable pour stocker la langue.
+    //Creation of a variable to store the language.
     public static ResourceManager? _rm;
     private string language = "EN";
 
     public LanguageControl()
     {
-    //Recherche du fichier de langue dans le projet.
+        //Recherche du fichier de langue dans le projet.
+        //Search for the language file in the project.
         _rm = new ResourceManager("Projet_Programmation_Système.Ressources.language", Assembly.GetExecutingAssembly());
     }
 
     // Recherche de la variable qui lui correspond en parametre en fonction de la langue.
+    // Search for the variable that corresponds to it in parameter depending on the language.
     public string? GetLanguageString(string name)
     {
         return _rm?.GetString(name);
     }
     //Création d'une méthode pour changer la langue.
+    //Creation of a method to change the language.
     public void ChangeLanguage()
     {
         AskLanguage();
         //Laisse une ligne vide
+        //Leaves an empty line
         ConsoleManager.DisplayEmptyLine();
         try
         {
             CultureInfo cultureInfo = new CultureInfo(language);
             CultureInfo.CurrentCulture = cultureInfo;
             CultureInfo.CurrentUICulture = cultureInfo;
-            //Affiche "ChangeLanguageSuccess" si le changement de langue est un succès.
+            //Affiche si le changement de langue est un succès.
+            //Displays if the language change is successful.
             ConsoleManager.DisplayLanguage("ChangeLanguageSuccess"); 
         }
         catch (Exception)
         {
         ConsoleManager.DisplayLanguage("ChangeLanguageError");
-            //Affiche "ChangeLanguageError" si le changement de langue est un echec.
+            //Affiche si le changement de langue est un echec.
+            //Displays if the language change is a failure.
         }
     }
     //Méthode qui vise à obtenir le choix de l'utilisateur pour la langue.
+    //Method that aims to obtain the user's choice for the language.
     private void AskLanguage()
     {
         bool isValid = false;
@@ -59,18 +68,21 @@ public class LanguageControl
             switch (ConsoleManager.GetInput())
             {
                 //Si l'utilisateur appuie sur "1", la langue sera le français.
+                //If the user presses "1", the language will be French.
                 case "1":
                     ConsoleManager.Display("FR");
                     language = "fr";
                     break;
 
                 //Si l'utilisateur appuie sur "2", la langue sera l'anglais.
+                //If the user presses "2", the language will be English.
                 case "2":
                     ConsoleManager.Display("EN");
                     language = "en";
                     break;
 
                 //Si l'utilisateur appuie sur une autre touche, une erreur s'affiche.
+                //If the user presses another key, an error appears.
                 default:
                     ConsoleManager.DisplayLanguage("InvalidInput");
                     language = "error";
