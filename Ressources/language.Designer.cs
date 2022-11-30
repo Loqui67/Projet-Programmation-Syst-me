e17ca642ad97b7aa40f480cdec68bad036952a51
Llianae <62105220+Llianae@users.noreply.github.com>
2022-11-30 10:09:22 +0100

    j'en ai marre

--- Ressources/language.Designer.cs
+++ Ressources/language.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Projet_Programmation_Système.Ressources {
+namespace AppWPF.Ressources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Projet_Programmation_Système.Ressources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Projet_Programmation_Système.Ressources.language", typeof(language).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AppWPF.Ressources.language", typeof(language).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
