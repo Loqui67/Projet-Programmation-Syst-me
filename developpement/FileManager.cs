dffa0d5de8ff342289c6b9205a4e416680b63749
Loqui67 <loicwalter88@gmail.com>
2022-12-02 18:07:15 +0100

    uncomment try/catch

--- developpement/FileManager.cs
+++ developpement/FileManager.cs
@@ -51,18 +51,18 @@ namespace Projet_Programmation_Système.developpement
         public static List<BackupJob>? ReadBackupJobFile()
         {
             CreateFileIfNotExist(backupJobJsonFileName);
-            //try
-            //{
+            try
+            {
                 using FileStream fs = File.OpenRead(backupJobJsonFileName);
                 byte[] jsonBytes = new byte[fs.Length];
                 fs.Read(jsonBytes, 0, jsonBytes.Length);
                 string jsonString = Encoding.UTF8.GetString(jsonBytes);
                 return JsonSerializer.Deserialize<List<BackupJob>>(jsonString);
-            //}
-            //catch (Exception)
-            //{
-            //    return new List<BackupJob>();
-            //}
+            }
+            catch (Exception)
+            {
+                return new List<BackupJob>();
+            }
         }
 
         //ecrit dans le fichier les logs journalières
