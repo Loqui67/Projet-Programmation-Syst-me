e75499e4dd22ac1192561b345fb2efeb383106b8
Paul Claus <paul.claus@viacesi.fr>
2022-12-13 14:53:10 +0100

    **

--- Ressources/language.Designer.cs
+++ Ressources/language.Designer.cs
@@ -22,14 +22,14 @@ namespace AppWPF.Ressources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Language {
+    internal class language {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Language() {
+        internal language() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace AppWPF.Ressources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AppWPF.Ressources.Language", typeof(Language).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AppWPF.Ressources.language", typeof(language).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
